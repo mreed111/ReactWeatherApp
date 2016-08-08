58bc4facb81c206c371ea673a26e16a7352c6235
Mike Reed <mike.reed.vallejo@gmail.com>
2016-08-07 19:32:47 -0700

    add horoku usage updates

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 //create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (reqest, response, next) {
+  if (request.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    response.redirect('http://' + request.hostname + request.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on Port 3000');
+app.listen(PORT, function () {
+  console.log('Express server is up on Port ' + PORT);
 });
